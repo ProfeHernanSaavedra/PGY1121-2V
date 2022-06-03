03fb995901ea0ea4e4772bd47363103dd6f40942
USRVI-LC2 <USRVI-LC2@duoc.acad>
2022-06-03 17:15:49 -0400

    se separo el archivo de funciones

--- ejercicio.py
+++ ejercicio.py
@@ -1,34 +1,6 @@
-def calcularIva(precio):
-    precio = precio * 0.19
-    return precio
 
-def calculoDesc(precio,descuento):
-    total = (precio - (precio * (descuento/100)))
-    print("El total a pagar con un descuento del ",descuento,"% es: $",total)
-
-def calculoIMC(peso, estatura):
-    imc = (peso/(estatura**2))
-    
-    if imc < 18.5:
-        print("Bajo peso")
-    else:
-        if imc >=18.5 and imc <= 24.9:
-            print("Peso adecuado")
-        else:
-            if imc >= 25 and imc <= 29.9:
-                print("Sobrepeso") 
-            else:
-                if imc >=30 and imc <=34.9:
-                    print("Obesidad grado 1")
-                else:
-                    if imc >=35 and imc <= 39.9:
-                        print("Obesidad grado 2")
-                    else:
-                        if imc >40 :
-                            print("Obesidad grado 3")
-                            
-
-
+import funciones as fn
+  
 op = 3
 
 while op != 4:
@@ -42,15 +14,15 @@ while op != 4:
     
     if op == 1:
         precio = int(input("Ingrese precio del producto: "))
-        iva = calcularIva(precio)
+        iva = fn.calcularIva(precio)
         print("El IVA del precio $:",precio,"es $",iva)
         
     if op == 2:
         precio = int(input("Ingrese precio del producto: "))
         descuento = int(input("Ingerse el % de descuento (0-100): "))
-        calculoDesc(precio,descuento)
+        fn.calculoDesc(precio,descuento)
     
     if op == 3:
         peso = float(input("Ingrese su peso en Kg: "))
         estatura = float(input("Ingrese estatura en metros: "))
-        calculoIMC(peso,estatura)
+        fn.calculoIMC(peso,estatura)
